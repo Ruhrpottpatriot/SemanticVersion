c685f0fd40dc1c67b50b5e3248cb4fb527dd9398
Robert Logiewa <Ruhrpottpatriot@gmx.de>
2018-04-09 14:29:13 +0200

    Change Targetframework to >netstandard1.2

---
+++ test/SemanticVersionTest/Parser/RangeParserTests.cs
@@ -0,0 +1,22 @@
+namespace SemanticVersionTest.Parser
+{
+    using Xunit;
+
+    public class RangeParserTests
+    {
+        [Fact]
+        public void ThrowArgumentException_NullString()
+        {
+        }
+
+        [Fact]
+        public void ThrowArgumentException_EmptyString()
+        {            
+        }
+
+        [Fact]
+        public void Success_WildcardOnly()
+        {            
+        }
+    }
+}
