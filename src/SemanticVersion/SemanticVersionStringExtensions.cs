e5b7c5df109c64cb4e06c7aedb939f60a99ad9d6
Dmitrii Frolov <steel835@gmail.com>
2021-02-12 01:01:14 +0400

    Updated prerelease component comparer to take wildcards into account

--- src/SemanticVersion/SemanticVersionStringExtensions.cs
+++ src/SemanticVersion/SemanticVersionStringExtensions.cs
@@ -5,41 +5,65 @@ namespace SemVersion
     /// <summary>Contains extensions to the string class to improve comparison.</summary>
     internal static class SemanticVersionStringExtensions
     {
-        /// <summary>Compares two component parts for equality.</summary>
-        /// <param name="component"> The left part to compare.</param>
-        /// <param name="other">The right part to compare.</param>
-        internal static int CompareComponent(this string component, string other)
+        /// <summary>Compares two component parts for equality. '*' character is treated as a wildcard</summary>
+        /// <remarks>
+        /// Comparison is made in accordance to <see href="https://semver.org/#spec-item-11">semantic version specification</see>
+        /// with an addition that prerelease component can be a wildcard ('*' character) that is equal to any <i>existing</i> prerelease component.
+        /// Wildcards in component's dot-separated identifiers are not supported.
+        /// </remarks>
+        /// <param name="left"> The left part to compare.</param>
+        /// <param name="right">The right part to compare.</param>
+        /// <returns>
+        ///     A signed number indicating the relative values of this instance and <paramref name="right"/>.
+        ///     <list type="table">
+        ///         <listheader>
+        ///             <term>Return value</term>
+        ///             <description>Description</description>
+        ///         </listheader>
+        ///         <item>
+        ///             <term> &lt; 0 </term>
+        ///             <description>This instance is less than <paramref name="right"/>.</description>
+        ///         </item><item>
+        ///             <term> = 0 </term>
+        ///             <description>This instance is equal to <paramref name="right"/> (or one of the instances is a wildcard).</description>
+        ///         </item><item>
+        ///             <term> &gt; 0 </term>
+        ///             <description>This instance is greater than <paramref name="right"/>.</description>
+        ///         </item>
+        ///     </list>
+        /// </returns>
+        internal static int CompareComponent(this string left, string right)
         {
-            var componentEmpty = string.IsNullOrWhiteSpace(component);
-            var otherEmpty = string.IsNullOrWhiteSpace(other);
+            var isLeftEmpty = string.IsNullOrWhiteSpace(left);
+            var isRightEmpty = string.IsNullOrWhiteSpace(right);
 
-            if ((componentEmpty && otherEmpty) || (component == "*" && other == "*"))
+            if ((isLeftEmpty && isRightEmpty) || left == "*" || right == "*")
             {
                 return 0;
             }
 
-            if (componentEmpty || component == "*")
+            if (isLeftEmpty)
             {
                 return +1;
             }
 
-            if (otherEmpty || other == "*")
+            if (isRightEmpty)
             {
                 return -1;
             }
 
-            var componentParts = component.Split(new[] { '.' }, StringSplitOptions.RemoveEmptyEntries);
-            var otherParts = other.Split(new[] { '.' }, StringSplitOptions.RemoveEmptyEntries);
+            var leftParts = left.Split(new[] { '.' }, StringSplitOptions.RemoveEmptyEntries);
+            var rightParts = right.Split(new[] { '.' }, StringSplitOptions.RemoveEmptyEntries);
 
-            for (var i = 0; i < Math.Min(componentParts.Length, otherParts.Length); i++)
+            for (var i = 0; i < Math.Min(leftParts.Length, rightParts.Length); i++)
             {
-                var componentChar = componentParts[i];
-                var otherChar = otherParts[i];
+                var leftChar = leftParts[i];
+                var rightChar = rightParts[i];
 
-                var componentIsNum = int.TryParse(componentChar, out var componentNumVal);
-                var otherIsNum = int.TryParse(otherChar, out var otherNumVal);
+                var leftIsNum = int.TryParse(leftChar, out var componentNumVal);
+                var rightIsNum = int.TryParse(rightChar, out var otherNumVal);
 
-                if (componentIsNum && otherIsNum)
+                if (leftIsNum && rightIsNum)
                 {
                     if (componentNumVal.CompareTo(otherNumVal) == 0)
                     {
@@ -48,24 +72,24 @@ namespace SemVersion
                     return componentNumVal.CompareTo(otherNumVal);
                 }
 
-                if (componentIsNum)
+                if (leftIsNum)
                 {
                     return -1;
                 }
 
-                if (otherIsNum)
+                if (rightIsNum)
                 {
                     return 1;
                 }
 
-                var comp = string.Compare(componentChar, otherChar, StringComparison.OrdinalIgnoreCase);
+                var comp = string.Compare(leftChar, rightChar, StringComparison.OrdinalIgnoreCase);
                 if (comp != 0)
                 {
                     return comp;
                 }
             }
 
-            return componentParts.Length.CompareTo(otherParts.Length);
+            return leftParts.Length.CompareTo(rightParts.Length);
         }
     }
 }
