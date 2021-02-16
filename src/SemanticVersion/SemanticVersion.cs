39d8814d0d81a0f992497eaefa5f8b87bf6fab48
Dmitrii Frolov <steel835@gmail.com>
2021-02-17 00:10:29 +0400

    Added validation on construction

--- src/SemanticVersion/SemanticVersion.cs
+++ src/SemanticVersion/SemanticVersion.cs
@@ -3,17 +3,26 @@
     using System;
     using System.Collections.Generic;
     using System.Globalization;
-    using System.Text;
+	using System.Linq;
+	using System.Text;
     using System.Text.RegularExpressions;
 
 
     /// <summary>Represents a version object, compliant with the Semantic Version standard 2.0 (http://semver.org)</summary>
     public class SemanticVersion : IEquatable<SemanticVersion>, IComparable<SemanticVersion>
     {
+        private const string WildcardSymbol = "*";
+
         private static IComparer<SemanticVersion> comparer = new VersionComparer();
 
-        private static readonly Regex VersionExpression = new Regex(@"^(?<major>[0]|[1-9]+[0-9]*|[*])((\.(?<minor>[0]|[1-9]+[0-9]*|[*]))(\.(?<patch>[0]|[1-9]+[0-9]*|[*]))?)?(\-(?<pre>[0-9A-Za-z\-\.]+|[*]))?(\+(?<build>[0-9A-Za-z\-\.]+|[*]))?$",
+        private static readonly Regex VersionExpression = new Regex(
+            @"^(?<major>[0]|[1-9]+[0-9]*|[*])((\.(?<minor>[0]|[1-9]+[0-9]*|[*]))(\.(?<patch>[0]|[1-9]+[0-9]*|[*]))?)?(\-(?<pre>[0-9A-Za-z\-\.]+|[*]))?(\+(?<build>[0-9A-Za-z\-\.]+|[*]))?$",
             RegexOptions.CultureInvariant | RegexOptions.ExplicitCapture);
+
+        private static readonly Regex PrereleaseExpression = new Regex(@"[0-9A-Za-z\-\.]+|[*]", RegexOptions.CultureInvariant);
+        private static readonly Regex BuildExpression = new Regex(@"[0-9A-Za-z\-\.]+|[*]", RegexOptions.CultureInvariant);
+
+
 
         /// <summary>Initializes a new instance of the <see cref="SemanticVersion"/> class.</summary>
         /// <param name="major">The major version component. <see langword="null"/> is treated as a '*' wildcard.</param>
@@ -22,16 +31,26 @@
         /// <param name="prerelease">The pre-release version component. Supports '*' wildcard (but not in dot-separated identifiers).</param>
         /// <param name="build">The build version component.</param>
         public SemanticVersion(int? major, int? minor, int? patch, string prerelease = "", string build = "")
-        {
-            this.Major = major;
-            this.Minor = minor;
-            this.Patch = patch;
-            this.Prerelease = prerelease;
-            this.Build = build;
-        }
+		{
+			CheckMainComponent(major, nameof(major));
+			CheckMainComponent(minor, nameof(minor));
+			CheckMainComponent(patch, nameof(patch));
+            CheckPrerelease(prerelease);
+            CheckBuild(build);
 
-        /// <summary>Gets the major version component. <see langword="null"/> is a '*' wildcard.</summary>
-        public int? Major { get; }
+            CheckWildcardOrder(major, minor, patch, prerelease);
+
+			this.Major = major;
+			this.Minor = minor;
+			this.Patch = patch;
+			this.Prerelease = prerelease;
+			this.Build = build;
+		}
+
+
+
+		/// <summary>Gets the major version component. <see langword="null"/> is a '*' wildcard.</summary>
+		public int? Major { get; }
 
         /// <summary>Gets the minor version component. <see langword="null"/> is a '*' wildcard.</summary>
         public int? Minor { get; }
@@ -45,40 +64,42 @@
         /// <summary>Gets the build version component.</summary>
         public string Build { get; }
 
-        public static bool operator ==(SemanticVersion left, SemanticVersion right)
-        {
-            return comparer.Compare(left, right) == 0;
-        }
 
-        public static bool operator !=(SemanticVersion left, SemanticVersion right)
-        {
-            return comparer.Compare(left, right) != 0;
-        }
 
-        public static bool operator <(SemanticVersion left, SemanticVersion right)
-        {
-            return comparer.Compare(left, right) < 0;
-        }
+		public static bool operator ==(SemanticVersion left, SemanticVersion right)
+		{
+			return comparer.Compare(left, right) == 0;
+		}
 
-        public static bool operator >(SemanticVersion left, SemanticVersion right)
-        {
-            return comparer.Compare(left, right) > 0;
-        }
+		public static bool operator !=(SemanticVersion left, SemanticVersion right)
+		{
+			return comparer.Compare(left, right) != 0;
+		}
 
-        public static bool operator <=(SemanticVersion left, SemanticVersion right)
-        {
-            return left == right || left < right;
-        }
+		public static bool operator <(SemanticVersion left, SemanticVersion right)
+		{
+			return comparer.Compare(left, right) < 0;
+		}
 
-        public static bool operator >=(SemanticVersion left, SemanticVersion right)
-        {
-            return left == right || left > right;
-        }
+		public static bool operator >(SemanticVersion left, SemanticVersion right)
+		{
+			return comparer.Compare(left, right) > 0;
+		}
+
+		public static bool operator <=(SemanticVersion left, SemanticVersion right)
+		{
+			return left == right || left < right;
+		}
+
+		public static bool operator >=(SemanticVersion left, SemanticVersion right)
+		{
+			return left == right || left > right;
+		}
+
 
         /// <summary>Implicitly converts a string into a <see cref="SemanticVersion"/>.</summary>
         /// <param name="versionString">The string to convert.</param>
         /// <returns>The <see cref="SemanticVersion"/> object.</returns>
-
         public static implicit operator SemanticVersion(string versionString)
         {
             // ReSharper disable once ArrangeStaticMemberQualifier
@@ -108,6 +129,45 @@
             return new SemanticVersion(major, minor, patch, string.Empty, build);
         }
 
+
+
+        private void CheckMainComponent(int? major, string componentName)
+        {
+            if (major.HasValue && major < 0)
+            {
+                throw new ArgumentOutOfRangeException(componentName, $"{componentName} version component can't be a negative number.");
+            }
+        }
+
+        private void CheckPrerelease(string prerelease)
+        {
+            if (!PrereleaseExpression.IsMatch(prerelease))
+            {
+                throw new ArgumentException("prerelease version component isn't compliant with the Semantic Versioning standard");
+            }
+        }
+
+        private void CheckBuild(string prerelease)
+        {
+            if (!BuildExpression.IsMatch(prerelease))
+            {
+                throw new ArgumentException("build version component isn't compliant with the Semantic Versioning standard");
+            }
+        }
+
+        private void CheckWildcardOrder(int? major, int? minor, int? patch, string prerelease)
+        {
+            var wildcards = new List<bool> { !major.HasValue, !minor.HasValue, !patch.HasValue, prerelease == WildcardSymbol };
+
+            var hasValueAfterWildcard = wildcards.SkipWhile(wildcard => !wildcard).Contains(false);
+
+            if (hasValueAfterWildcard)
+            {
+                throw new ArgumentException("Version components can't have values after a components with wildcards");
+            }
+        }
+
+
         /// <summary>Change the comparer used to compare two <see cref="SemanticVersion"/> objects.</summary>
         /// <param name="versionComparer">An instance of the comparer to use in future comparisons.</param>
         public static void ChangeComparer(IComparer<SemanticVersion> versionComparer) => comparer = versionComparer;
@@ -120,6 +180,7 @@
         /// <param name="inputString">The string to check for validity.</param>
         /// <returns>True, if the passed string is a valid <see cref="SemanticVersion"/>, otherwise false.</returns>
         public static bool IsVersion(string inputString) => VersionExpression.IsMatch(inputString);
+
 
         /// <summary>Parses the specified string to a semantic version.</summary>
         /// <param name="versionString">The version string.</param>
@@ -219,6 +280,8 @@
             return true;
         }
 
+
+
         /// <inheritdoc />
         public bool Equals(SemanticVersion other) => comparer.Compare(this, other) == 0;
 
@@ -252,14 +315,14 @@
 
             if (!this.Major.HasValue)
             {
-                return "*";
+                return WildcardSymbol;
             }
 
             builder.Append($"{this.Major.Value}.");
 
             if (!this.Minor.HasValue)
             {
-                builder.Append("*");
+                builder.Append(WildcardSymbol);
                 return builder.ToString();
             }
 
@@ -267,7 +330,7 @@
 
             if (!this.Patch.HasValue)
             {
-                builder.Append("*");
+                builder.Append(WildcardSymbol);
                 return builder.ToString();
             }
 
